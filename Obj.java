6b16666775e8fff7adbd8800cf08616a1c6a36e1
MarkPreschern <mark.preschern@gmail.com>
2018-02-19 21:39:16 -0500

    adding 1 more file

---
+++ Obj.java
@@ -0,0 +1,72 @@
+package Evolution;
+
+public class Obj
+{
+    private String name;
+    private double[] x, y, w, t;
+    private double dx, dy;
+    private double distance;
+    private int evolution;
+    public Obj(String name, double[] x, double[] y, double[] w, double[] t, double dx, double dy, double distance, int evolution)
+    {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.t = t;
+        this.dx = dx;
+        this.dy = dy;
+        this.distance = distance;
+        this.evolution = evolution;
+    }
+    public String getName(){
+        return name;}
+    public double[] getX(){
+        return x;}
+    public double[] getY(){
+        return y;}
+    public double getW(int value){
+        return w[value];}
+    public double getT(int value){
+        return t[value];}
+    public double getDX(){
+        return dx;}
+    public double getDY(){
+        return dy;}
+    public double getDistance(){
+        return distance;}
+    public int getEvolution(){
+        return evolution;}
+    public double getX(int value){
+        return x[value];}
+    public double getY(int value){
+        return y[value];}
+    public int getSize(){
+        return x.length;}
+    public void addX(double value){
+        for(int i=0;i<getSize();i++)
+            x[i] += value;}
+    public void addY(double value){
+        for(int i=0;i<getSize();i++)
+            y[i] += value;}
+    public void setX(double[] value){
+        x = value;}
+    public void setY(double[] value){
+        y = value;}
+    public void setW(double[] value){
+        w = value;}
+    public void setT(double[] value){
+        t = value;}
+    public void setDX(double value){
+        dx = value;}
+    public void setDY(double value){
+        dy = value;}
+    public void addDX(double value){
+        dx += value;}
+    public void addDY(double value){
+        dy += value;}
+    public void setDistance(double value){
+        distance = value;}
+    public void addEvolution(){
+        evolution++;}
+}
